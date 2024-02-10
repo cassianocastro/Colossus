d86f92c131204e7917b1d793f2b245a53de4cd86
Cassiano Castro <development.castro@gmail.com>
2024-02-10 18:54:15 -0300

    Refactor: moved std headers inclusions to user-defined header

--- src/app/Colossus.c
+++ src/app/Colossus.c
@@ -1,9 +1,3 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdbool.h>
-#include <iso646.h>
-#include <string.h>
-#include <ctype.h>
 #include "../includes/Colossus.h"
 
 const uint ONE_MILLION = 1000000;
